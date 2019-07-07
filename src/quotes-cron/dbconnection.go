0a5ede4c3c82f38598b80f4d84a78a9f8c800c29
bryntsev-ae <bryntsev-ae@fxclub.org>
2019-07-07 20:28:46 +0200

    Added quotes-cron service

---
+++ src/quotes-cron/dbconnection.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"context"
+	"time"
+	//"encoding/json"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/bson"
+)
+
+func db_connect() *mongo.Client{
+	client, err := mongo.NewClient(options.Client().ApplyURI(Config.Hosts.Mongodb))
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	if err = client.Connect(ctx); err != nil {log.Fatal(err)}
+	return client
+}
+
+func isElementInDB(currency Quote) bool {
+	var result []* Quote
+	client := db_connect()
+	collection := client.Database("Quotes").Collection("Currencies")
+	filter := bson.D{
+		{"symbol",  currency.Symbol}, 
+		{"category", currency.Category},
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	cur, err := collection.Find(ctx, filter)
+	if err != nil { log.Fatal(err) }
+	client.Disconnect(ctx)
+	for cur.Next(ctx) {
+		var elem Quote
+		err := cur.Decode(&elem)
+		if err != nil {log.Fatal(err)}
+		result = append(result, &elem)
+	}
+	if err := cur.Err(); err != nil {log.Fatal(err)}
+	cur.Close(ctx)
+	if len(result) >= 1 {
+		return true
+	}
+	return false
+}
+
+func updateRate(currency Quote){
+	client := db_connect()
+	collection := client.Database("Quotes").Collection("Currencies")
+	filter := bson.D{
+		{"symbol",  currency.Symbol}, 
+		{"category", currency.Category},
+	}
+	update := bson.D{
+		{"$set", bson.D{
+			{"rate", currency.Rate},
+		}},
+	}
+	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	_, err := collection.UpdateOne(ctx, filter, update)
+	if err != nil { log.Fatal(err) }
+	client.Disconnect(ctx)
+}
+
+func writeNewCurrency(currency Quote){
+	client := db_connect()
+	ctx,_:= context.WithTimeout(context.Background(), 30*time.Second)
+	collection := client.Database("Quotes").Collection("Currencies")
+	_, err := collection.InsertOne(ctx,currency)
+	if err != nil { log.Fatal(err) }
+	client.Disconnect(ctx)
+}
+
+func test_con(){
+	client := db_connect()
+	collection := client.Database("Quotes").Collection("Currencies")
+	filter := bson.D{}
+	
+	var cur *mongo.Cursor
+	ctx, err_ctx := context.WithTimeout(context.Background(), 30*time.Second); if err_ctx!=nil{log.Fatal(err_ctx); return}
+	cur, err := collection.Find(ctx,filter); if err != nil { log.Fatal(err) }
+	client.Disconnect(ctx)
+	cur.Close(ctx)
+	fmt.Printf("\n")
+	fmt.Printf("%v",cur)
+}
