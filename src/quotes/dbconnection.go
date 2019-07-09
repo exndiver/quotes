d6c9d93ca3165d430034a8eaba262d016b9151d5
bryntsev-ae <bryntsev-ae@fxclub.org>
2019-07-09 10:56:31 +0200

    Again remove trash

--- src/quotes/dbconnection.go
+++ src/quotes/dbconnection.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"context"
-	"strings"
 	"time"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -41,66 +40,6 @@ func getAllElementsinMemory(){
 	}
 	if err := cur.Err(); err != nil {log.Fatal(err)}
 	cur.Close(ctx)
-}
-
-func getAllElements() []*Quote {
-	var result []* Quote
-	client := db_connect()
-	collection := client.Database("Quotes").Collection("Currencies")
-	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
-	cur, err := collection.Find(ctx, bson.D{})
-	if err != nil { log.Fatal(err) }
-	client.Disconnect(ctx)
-	for cur.Next(ctx) {
-		var elem Quote
-		err := cur.Decode(&elem)
-		if err != nil {log.Fatal(err)}
-		result = append(result, &elem)
-	}
-	if err := cur.Err(); err != nil {log.Fatal(err)}
-	cur.Close(ctx)
-	return result
-}
-
-func getOneGroup(Category int) []*Quote {
-	var result []* Quote
-	client := db_connect()
-	collection := client.Database("Quotes").Collection("Currencies")
-	filter := bson.D{
-		{"category", Category},
-	}
-	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
-	cur, err := collection.Find(ctx, filter)
-	if err != nil { log.Fatal(err) }
-	client.Disconnect(ctx)
-	for cur.Next(ctx) {
-		var elem Quote
-		err := cur.Decode(&elem)
-		if err != nil {log.Fatal(err)}
-		result = append(result, &elem)
-	}
-	if err := cur.Err(); err != nil {log.Fatal(err)}
-	cur.Close(ctx)
-	return result
-}
-
-
-
-func getOneElement(Category int, Symbol string) Quote {
-	var result Quote
-	client := db_connect()
-	collection := client.Database("Quotes").Collection("Currencies")
-	filter := bson.D{
-		{"category", Category},
-		{"symbol", strings.ToUpper(Symbol)},
-	}
-	ctx, _ := context.WithTimeout(context.Background(), 30*time.Second)
-	err := collection.FindOne(ctx, filter).Decode(&result)
-	if err != nil {
-		log.Panic(err)
-	}
-	client.Disconnect(ctx)
-	return result
 }
 
 func isElementInDB(currency Quote) bool {
