c3539d4e7f8a9e24615d45b4bfc2ceb6ee34a641
bryntsev-ae <bryntsev-ae@fxclub.org>
2019-07-26 21:24:42 +0200

    Some fix

---
+++ src/quotes/belarus.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// QuotesResponse - Responce from api with quotes
+type QuotesResponse struct {
+	CurOfficialRate float64 `json:"Cur_OfficialRate"`
+}
+
+func blrdRub() {
+	var quotes QuotesResponse
+	resp, err := http.Get("http://www.nbrb.by/API/ExRates/Rates/292?Periodicity=0")
+
+	if err != nil {
+		Logger2Errors("Error importing from blrd bank")
+	}
+
+	Logger2("Belarus Rub was imported")
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if err := json.Unmarshal(body, &quotes); err != nil {
+		fmt.Printf("%+s\n", err)
+		return
+	}
+	str := Quote{
+		Symbol:   "BYR",
+		Rate:     quotes,
+		Category: 0,
+	}
+	if isElementInDB(str) {
+		updateRate(str)
+	} else {
+		writeNewCurrency(str)
+	}
+}
--- src/quotes/crypto.go
+++ src/quotes/crypto.go
@@ -1,30 +1,32 @@
 package main
 
 import (
-	"net/http"
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"net/http"
 	"strconv"
-	"encoding/json"
 )
 
-type CrypResp struct{
-	Ticker	Cryp `json:"ticker"`
+type CrypResp struct {
+	Ticker Cryp `json:"ticker"`
 }
 
 type Cryp map[string]string
 
-func getCrypto(){
+func getCrypto() {
 	var C CrypResp
-	for _, v := range Config.Cryptoapilist{
+	for _, v := range Config.Cryptoapilist {
 		resp, err := http.Get(v)
 		if err != nil {
 			Logger2Errors("Error calling crypto api for %s" + v)
+			return
 		}
 		Logger2("Was loaded successfully " + v)
-		body,err := ioutil.ReadAll(resp.Body)
-		if err != nil{
-			Logger2Errors("Error getting responce body "+v)
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			Logger2Errors("Error getting responce body " + v)
+			return
 		}
 		if err := json.Unmarshal(body, &C); err != nil {
 			Logger2Errors("Error parsing JSON for " + v)
@@ -33,11 +35,11 @@ func getCrypto(){
 		}
 		var s, e = strconv.ParseFloat(C.Ticker["price"], 64)
 		if e != nil {
-			Logger2Errors("Error parsing the price for "+v)
+			Logger2Errors("Error parsing the price for " + v)
 		}
-		var q = Quote {
-			Symbol: C.Ticker["base"],
-			Rate: (1/s),
+		var q = Quote{
+			Symbol:   C.Ticker["base"],
+			Rate:     (1 / s),
 			Category: 1,
 		}
 		if isElementInDB(q) {
