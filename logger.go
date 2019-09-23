6d73203950f5f2d7c4539124be7359d0d6fe31d9
bryntsev-ae <bryntsev-ae@fxclub.org>
2019-09-23 18:28:15 +0200

    Enable logging

--- logger.go
+++ logger.go
@@ -9,19 +9,19 @@ import (
 
 // Logger1 - Access logs
 func Logger1(r *http.Request) {
-	/*	f, err := os.OpenFile("./logs/logs.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
-		if err != nil {
-			log.Fatalf("error opening file: %v", err)
-		}
-		log.SetOutput(f)
-		log.Printf(
-			"%s\t%s\t%s\t%s\t",
-			r.Method,
-			r.RequestURI,
-			r.Header,
-			r.RemoteAddr,
-		)
-		f.Close()*/
+	f, err := os.OpenFile("./logs/logs.log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	if err != nil {
+		log.Fatalf("error opening file: %v", err)
+	}
+	log.SetOutput(f)
+	log.Printf(
+		"%s\t%s\t%s\t%s\t",
+		r.Method,
+		r.RequestURI,
+		r.Header,
+		r.RemoteAddr,
+	)
+	f.Close()
 }
 
 // Logger1Errors - Errors logs for api request
